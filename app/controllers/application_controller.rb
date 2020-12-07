4d3a3fcd2e15e329f9cc237f7cdf5ef08bfa4341
thedanielleellis <thedanielleellis@gmail.com>
2020-12-07 13:07:16 -0600

    created some test routes to update app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     private
 
     def logged_in?
-        !!session[:user_id]
+        !!session[:dancer_id]
     end
 
     def current_dancer
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
 
-get '/' => 'sessions#welcome'
+get '/' => 'static#index'
 get '/login' => 'sessions#new'
 post '/login' => 'sessions#create'
 get '/signup' => 'dancers#new'
-post '/signup' => 'dancers#create'
-
 delete '/logout' => 'sessions#destroy'
 
 get '/auth/:provider/callback' => 'sessions#create'
-
+  
+  resources :dancers
+  resources :categories, only: [:new, :index, :create]
   resources :comments
   resources :dance_classes do
     resources :comments, only: [:new, :index]  
   end
 
-  resources :dancers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
