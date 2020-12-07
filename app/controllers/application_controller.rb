9d65f206c243a009f2c9c1f9ca40d28a4f3b95aa
thedanielleellis <thedanielleellis@gmail.com>
2020-12-07 13:10:32 -0600

    created current user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_dancer, :logged_in?
+    helper_method :current_dancer, :logged_in?, :create_session
 
     private
+
+    def require_login
+        redirect_to login_path unless session[:user_id]
+    end
 
     def logged_in?
         !!session[:dancer_id]
@@ -11,6 +15,10 @@ class ApplicationController < ActionController::Base
     def current_dancer
         @current_dancer ||= Dancer.find_by_id(session[:dancer_id]) if session[:dancer_id]
     end
+
+    def create_session
+        session[:dancer_id] = @dancer.id
+      end
 
     def redirect_if_not_logged_in
         redirect_to login_path if !logged_in?
