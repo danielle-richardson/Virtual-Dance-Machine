1e3d0ae60376c7835face1f2045f4a9061be99c0
thedanielleellis <thedanielleellis@gmail.com>
2020-12-07 00:23:10 -0600

    updated models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
 
     has_many :dance_classes
+    validates :name, presence: true, uniqueness: true
+
+    scope :alpha -> {order(:name)}
 end
--- app/models/dancer.rb
+++ app/models/dancer.rb
@@ -1,11 +1,25 @@
 class Dancer < ApplicationRecord
-
     has_many :comments 
     has_many :commented_dance_classes, through: :comments, source: :dance_class 
     has_many :dance_classes
     
     validates :username, uniqueness: true, presence: true   
-    
+    validates :email, presence: true
+
     has_secure_password 
+
+
+  def self.create_by_google_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.password = SecureRandom.hex
+    end
+  end
+
+  def self.create_by_github_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.password = SecureRandom.hex
+
+    end
+  end
 
 end
